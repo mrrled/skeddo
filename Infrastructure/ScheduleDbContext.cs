a2f380c9d6b68edab38d051927471df97c201648
Алсу Исламова <161420770+mrrled@users.noreply.github.com>
2025-10-12 17:24:21 +0500

    добавление appsettings.json, настройка DI

---
+++ Infrastructure/ScheduleDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ScheduleDbContext : DbContext
+{
+    public ScheduleDbContext(DbContextOptions<ScheduleDbContext> options) : base(options)
+    {
+    }
+}
