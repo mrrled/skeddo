285cda11d38752d2e93179a811c788db474654ca
Алсу Исламова <161420770+mrrled@users.noreply.github.com>
2025-11-16 17:16:36 +0500

    add validation

--- Domain/Models/Lesson.cs
+++ Domain/Models/Lesson.cs
@@ -3,18 +3,23 @@ namespace Domain.Models;
 public class Lesson(int id,
     SchoolSubject subject,
     LessonNumber lessonNumber,
-    Teacher teacher,
-    StudyGroup studyGroup,
-    Classroom classroom,
+    Teacher? teacher,
+    StudyGroup? studyGroup,
+    Classroom? classroom,
     string comment = "",
-    bool isConflict = false
+    WarningType warningType = WarningType.Normal
     ) : Entity<int>(id)
 {
     public SchoolSubject Subject { get; } = subject;
-    public LessonNumber LessonNumber { get; } = lessonNumber;
-    public Teacher Teacher { get; } = teacher;
-    public StudyGroup StudyGroup { get; } = studyGroup;
-    public Classroom Classroom { get; } = classroom;
+    public LessonNumber? LessonNumber { get; } = lessonNumber;
+    public Teacher? Teacher { get; } = teacher;
+    public StudyGroup? StudyGroup { get; } = studyGroup;
+    public Classroom? Classroom { get; } = classroom;
     public string Comment { get; } = comment;
-    public bool IsConflict { get; } = isConflict;
+    public WarningType WarningType { get; private set; } = warningType;
+    //хранить список с кем конфликтует?
+    internal void SetWarningType(WarningType warningType)
+    {
+        WarningType = warningType;
+    }
 }
