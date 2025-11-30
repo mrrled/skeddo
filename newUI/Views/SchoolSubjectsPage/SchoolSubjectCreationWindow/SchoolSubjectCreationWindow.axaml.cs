04888e4d7a3db61709315e4fff2ffbbd140c55fb
vovchekkk <vlgavrikov06@gmail.com>
2025-11-30 17:36:46 +0500

    add school_subjects_page

---
+++ newUI/Views/SchoolSubjectsPage/SchoolSubjectCreationWindow/SchoolSubjectCreationWindow.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using newUI.ViewModels.SchoolSubjectsPage.SchoolSubjects;
+
+namespace newUI.Views.SchoolSubjectsPage.SchoolSubjectCreationWindow;
+
+public partial class SchoolSubjectCreationWindow : Window
+{
+    public SchoolSubjectCreationWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void NameTextBox_TextChanging(object? sender, TextChangingEventArgs e)
+    {
+        // Получаем ViewModel
+        if (DataContext is SchoolSubjectCreationViewModel viewModel)
+        {
+            // Вызываем асинхронный метод SetName
+            if (sender is TextBox textBox)
+            {
+                // Не используйте await здесь, так как это обработчик события. 
+                // Просто запускаем Task.
+                viewModel.SetName(textBox.Text ?? string.Empty);
+            }
+        }
+    }
+}
