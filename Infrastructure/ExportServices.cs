61a3ae7c44b4491cfe3149c5acd00fdbfcc084c7
vovchekkk <vlgavrikov06@gmail.com>
2025-12-14 17:30:53 +0500

    add export_generator

---
+++ Infrastructure/ExportServices.cs
@@ -0,0 +1,10 @@
+﻿using Application.IServices;
+
+namespace Infrastructure;
+
+public class ExportServices(ExportGenerator exportGenerator) : IExportServices
+{
+    public async Task GeneratePdfAsync(int scheduleId) => await exportGenerator.GeneratePdfAsync(scheduleId);
+    
+    public async Task GenerateExcelAsync(int scheduleId) => await exportGenerator.GenerateExcelAsync(scheduleId);
+}
